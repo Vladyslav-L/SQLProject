b05ddf05b8c2dcc05b6063ee9265dc3d8839a2c1
Vlad Liashenko <pactur@ukr.net>
2021-05-26 14:06:12 +0300

    Добавьте файлы проекта.

---
+++ SQLProject1/SQLHelper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SQLProject
+{
+    class SqlHelper
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder;        
+        private SqlConnection _sqlConnection;
+
+        public SqlHelper (string dbName)
+        {
+            _sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "localhost",
+                InitialCatalog = dbName,
+                IntegratedSecurity = true
+            };
+        }
+
+        public void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            _sqlConnection.Open();
+        }
+
+        public void CloseConnection() => _sqlConnection.Close();
+              
+        public void ExecuteNonQuery(string request)
+        {
+            var command = new SqlCommand(request, _sqlConnection);
+            command.ExecuteNonQuery();
+        }
+
+        public void Insert(string table, Dictionary<string, string> parameters)
+        {
+            var columns = string.Empty;
+            var values = string.Empty;
+
+            foreach (var (key, value) in parameters)
+            {
+                columns += $"{key},";
+                values += $"{value},";
+            }
+
+            var command = new SqlCommand($"insert into {table} ({columns.TrimEnd(',')}) values ({values.TrimEnd(',')})",
+               _sqlConnection);
+            command.ExecuteNonQuery();
+        }
+
+        public void Delete (string table, Dictionary<string, string> parameters)
+        {
+            var whereConditions = string.Empty;
+
+            foreach (var (key, value) in parameters)
+                whereConditions += $" and {key}={value}";           
+
+            var command = new SqlCommand($"delete from {table} where {whereConditions.Substring(5)}",
+               _sqlConnection);            
+            command.ExecuteNonQuery();   
+        }
+
+        public void Update (string table, Dictionary<string, string> parameters, Dictionary<string, string> condition)
+        {
+            var whereConditions = string.Empty;
+
+            foreach (var (key, value) in condition)
+                whereConditions += $" and {key}={value}"; 
+
+             var whereParameters = string.Empty;
+
+            foreach (var (column, value) in parameters)
+                whereParameters += $", {column}={value}"; 
+            
+
+            var command = new SqlCommand($"update {table} set {whereParameters[2..]} where {whereConditions.Substring(5)}",
+               _sqlConnection);            
+            command.ExecuteNonQuery();   
+        }
+
+        public bool IsRowExistedInTable(string table, Dictionary<string, string> parameters)
+        {
+            var whereParameters = string.Empty;
+
+            foreach (var (key, value) in parameters)
+                whereParameters += $" and {key}={value}";
+
+            var sqlDataAdapter = new SqlDataAdapter(
+                $"select * from {table} where {whereParameters.Substring(5)}",
+                _sqlConnection);
+            var dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+
+            return dataTable.Rows.Count > 0;
+        }
+    }
+}
---
+++ SQLProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace SQLProject
+{
+    public class Tests
+    {
+        private SqlHelper _sqlHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sqlHelper = new SqlHelper("Shop");
+            _sqlHelper.OpenConnection();
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            _sqlHelper.Delete("[Shop].[dbo].[Products]", new Dictionary<string, string> { { "Name", "'TestName'" } });
+            _sqlHelper.Delete("[Shop].[dbo].[Products]", new Dictionary<string, string> { { "Name", "'TestUpdate'" } });
+            _sqlHelper.CloseConnection();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _sqlHelper.Insert("Products",
+                    new Dictionary<string, string> { { "Id", "1" }, { "Name", "'TestName'" }, { "Count", "44" } });
+            var result = _sqlHelper.IsRowExistedInTable("Products",
+                new Dictionary<string, string> { { "Id", "1" }, { "Name", "'TestName'" }, { "Count", "44" } });
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void CheckUpdate()
+        {
+            _sqlHelper.Update("Products", new Dictionary<string, string> { { "Id", "5" }, { "Name", "'TestUpdate'" }, { "Count", "10" } },
+                new Dictionary<string, string> { { "Count", "234" } }
+                );
+            var result = _sqlHelper.IsRowExistedInTable("Products",
+               new Dictionary<string, string> { { "Id", "5" }, { "Name", "'TestUpdate'" }, { "Count", "10" } });
+
+            Assert.True(result);
+        }
+    }
+}
